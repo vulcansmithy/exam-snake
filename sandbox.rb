ef3b485f6e2f3938fd6cae6f97032892bc77c9cc
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2021-04-12 23:26:38 +0800

    WIP: enable curses in sandbox.

--- sandbox.rb
+++ sandbox.rb
@@ -2,13 +2,15 @@ require "./sandbox.rb"
 require "./snake.rb"
 
 require "awesome_print"
+require "curses"
+require "logger"
 
 # this enable this ruby script to be run from the command line
 Sandbox.new.main if __FILE__ == $PROGRAM_NAME
 
 class Sandbox
 
-  def main
+  def exp1
     snake = Snake.new
     snake.x_pos = 0
     snake.y_pos = 0
@@ -27,6 +29,34 @@ class Sandbox
     snake.turn_right
     snake.report
   end
+  
+  def main
+    
+
+    Curses.init_screen
+    begin
+      x = Curses.cols / 2  # We will center our text
+      y = Curses.lines / 2
+  
+      Curses.setpos(y, x)  # Move the cursor to the center of the screen
+      Curses.addstr("Hello World")  # Display the text
+      Curses.refresh  # Refresh the screen
+      Curses.getch  # Waiting for a pressed key to exit
+  
+      Curses.setpos(y + 1, x)  # Move the cursor to the center of the screen
+      Curses.addstr("Ping!")  # Display the text
+    # Curses.refresh  # Refresh the screen
+      Curses.getch  # Waiting for a pressed key to exit
+    
+      Curses.setpos(y + 2, x)  # Move the cursor to the center of the screen
+      Curses.addstr("Pong!")  # Display the text
+      Curses.refresh  # Refresh the screen
+
+      Curses.getch  # Waiting for a pressed key to exit
+    ensure
+      Curses.close_screen
+    end
+  end  
   
 end
 
