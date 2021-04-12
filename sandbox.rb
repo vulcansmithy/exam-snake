b1a3f5f5e96f5386fa04bf53d9a57b041e13bb57
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2021-04-13 00:36:45 +0800

    WIP: figure out how to use 'wsad' keys.

--- sandbox.rb
+++ sandbox.rb
@@ -6,9 +6,13 @@ require "curses"
 require "logger"
 
 # this enable this ruby script to be run from the command line
-Sandbox.new.main if __FILE__ == $PROGRAM_NAME
+Sandbox.new.exp2 if __FILE__ == $PROGRAM_NAME
 
 class Sandbox
+  
+  def initialize
+    @log = Logger.new("sandbox-debug.log") 
+  end  
 
   def exp1
     snake = Snake.new
@@ -30,38 +34,79 @@ class Sandbox
     snake.report
   end
   
-  def main
-    
-
+  # figure out how to get input from the keyboard
+  def exp2
     Curses.init_screen
+    Curses.curs_set(0)
+    Curses.init_screen
+    Curses.start_color
+    Curses.noecho
+    begin
+      win = Curses.stdscr
+      win.nodelay = true
+      @log.debug "@DEBUG L:#{__LINE__}   win.class=#{win.class}"
+    
+    
+      x = win.maxx / 2
+      y = win.maxy / 2
+      win.setpos(y, x)
+      win.addstr("Enter key: ")
+      win.refresh
+      
+      input = nil    
+      while true
+        input = win.getch
+
+        if input == 'x'
+          @log.debug "@DEBUG L:#{__LINE__}   inputed 'x' input='#{input}'" 
+          break
+        elsif input == 'a'
+          @log.debug "@DEBUG L:#{__LINE__}   inputed 'a' input='#{input}'" 
+        elsif input == 'd'
+          @log.debug "@DEBUG L:#{__LINE__}   inputed 'd' input='#{input}'" 
+        elsif input == 'w'
+          @log.debug "@DEBUG L:#{__LINE__}   inputed 'w' input='#{input}'" 
+        elsif input == 's'
+          @log.debug "@DEBUG L:#{__LINE__}   inputed 's' input='#{input}'" 
+        end     
+             
+             
+      end 
+      
+    ensure
+      Curses.close_screen
+    end  
+  end  
+  
+  
+  def main
+    Curses.init_screen
+    Window.nodelay = true
+    
     begin
       x = Curses.cols / 2  # We will center our text
       y = Curses.lines / 2
-  
-      Curses.setpos(y, x)  # Move the cursor to the center of the screen
-      Curses.addstr("Hello World")  # Display the text
-      Curses.refresh  # Refresh the screen
-      Curses.getch  # Waiting for a pressed key to exit
-  
-      Curses.setpos(y + 1, x)  # Move the cursor to the center of the screen
-      Curses.addstr("Ping!")  # Display the text
-    # Curses.refresh  # Refresh the screen
-      Curses.getch  # Waiting for a pressed key to exit
-    
-      Curses.setpos(y + 2, x)  # Move the cursor to the center of the screen
-      Curses.addstr("Pong!")  # Display the text
-      Curses.refresh  # Refresh the screen
 
-      Curses.getch  # Waiting for a pressed key to exit
+      Curses.setpos(y, x)
+      Curses.addstr("Enter key: ")
+      Curses.refresh
+#      result = Curses.getch
+      
+#      @log.debug "@DEBUG L:#{__LINE__}   #{result}"
+#      @log.debug "@DEBUG L:#{__LINE__}   #{Curses::KEY_UP}"
+#      @log.debug "@DEBUG L:#{__LINE__}   #{Curses::KEY_DOWN}"
+
+      input = nil    
+      while true
+        result = Curses.getch
+
+        @log.debug "@DEBUG L:#{__LINE__}   input='#{input}'"
+        break if result == 'x'
+      end    
+
     ensure
       Curses.close_screen
     end
   end  
   
 end
-
-
-
-
-
-
